2f50191c01de29c87479dd4010f677563f21cd4b
viacheslav.zhygulin <viacheslav.zhygulin@xdev.re>
2019-03-22 16:41:57 +0200

    fixed hosts

--- models/resnet/tensorflow/train.sh
+++ models/resnet/tensorflow/train.sh
@@ -27,7 +27,7 @@ mpirun -np $gpus -mca plm_rsh_no_tree_spawn 1 \
 	-bind-to socket -map-by slot \
 	-x HOROVOD_HIERARCHICAL_ALLREDUCE=1 -x HOROVOD_FUSION_THRESHOLD=16777216 \
 	-x NCCL_MIN_NRINGS=4 -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib \
-	-x TF_CPP_MIN_LOG_LEVEL=0 \
+	-x TF_CPP_MIN_LOG_LEVEL=0 --allow-run-as-root \
 	python -W ignore train_imagenet_resnet_hvd.py \
 	--data_dir /opt/ml/input/data/training --log_dir /opt/ml/model/imagenet_resnet \
 	--num_epochs 90 -b $BATCH_SIZE \
